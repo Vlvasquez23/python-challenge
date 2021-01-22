d070d9baddefd6c21e98f978c1f7cc35ebe0bb1c
Vanessa Vasquez <vlvasquez23sa@gmail.com>
2021-01-21 21:57:25 -0600

    PyPoll_Final

--- PyPoll/pypoll-main.py
+++ PyPoll/pypoll-main.py
@@ -26,7 +26,7 @@ with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     csv_header = next(csvreader)
-    row = next(csvreader)
+    #row = next(csvreader) 
 
 
     for row in csvreader:
